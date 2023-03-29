f704c0cd95eea9e69b540edf883b1e84436384c7
Vladyslav Vdovychenko <vlad182ava@gmail.com>
2023-03-29 15:42:32 +0300

    SwiftUI project init

---
+++ ScanAR-iOS/ScanAR_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanAR_iOSApp.swift
+//  ScanAR-iOS
+//
+//  Created by Vladyslav Vdovychenko on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScanAR_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
